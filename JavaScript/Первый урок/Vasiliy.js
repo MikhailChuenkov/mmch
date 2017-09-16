2469e68cea41eb7a16d8815ca294fb7dcd3e43da
Mikhail <m.chuenkov@gmail.com>
2017-09-16 19:55:01 +0300

    выполнение первого ДЗ

---
+++ JavaScript/Первый урок/Vasiliy.js
@@ -0,0 +1,8 @@
+//Записываем в переменную Васиоия
+document.write ("<p>2. Теперь выводим имя админа</p>");
+var admin;
+var name;
+    name = "Василий";
+    admin = name;
+alert("Имя админ " + admin);
+
