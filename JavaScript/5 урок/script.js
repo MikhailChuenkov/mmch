42fa3f297275f929eecf8376bab8e713e9347f2f
Mikhail <m.chuenkov@gmail.com>
2017-10-02 23:46:14 +0300

    5 урок

---
+++ JavaScript/5 урок/script.js
@@ -0,0 +1,9 @@
+var text = document.createElement("span");
+//div.className = "black_cell";
+    //var divB = document.getElementsByClassName("black_cell");
+
+text.innerHTML = "Привет!";
+black.appendChild(text);
+/*div.style.border.color = "black";
+div.style.weidth = "10px";
+div.style.height = "10px";*/
